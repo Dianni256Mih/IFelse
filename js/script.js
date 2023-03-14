73c566449dbfacb59a088f1152891fcf450d4e6c
Dianni256Mih <emily256dianni@gmail.com>
2023-03-14 09:24:49 -0300

    Projeto mudar fundo finalizado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 let button = document.getElementById('button');
+let fundo_azul = false;
 
 function mudar_fundo(){
-    document.body.style.background = '#40E0D0';
+    if (fundo_azul == false){
+        fundo_azul = true;
+        document.body.style.background = '#7FFFD4';
+        button.style.background = '#FF82AB';
+        button.style.color = '#7FFFD4';
+    }else{
+        fundo_azul = false;
+        document.body.style.background = '#FF82AB';
+        button.style.background = '#7FFFD4';
+        button.style.color = '#FF82AB';
+    }
 }
